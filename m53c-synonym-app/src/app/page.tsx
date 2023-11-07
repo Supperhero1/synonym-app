0e9723251588ff62d84e6a7e1ad2e916943165f3
marko.petric <marko.petric@rimac-technology.com>
2023-11-07 16:08:58 +0100

    starting code

---
+++ m53c-synonym-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/page.module.scss'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Synonym app</h1>
+    </main>
+  )
+}
